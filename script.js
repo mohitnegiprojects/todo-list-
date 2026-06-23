abf5ae2e7ffff708d90b44c70baa759c12c644c3
Mohit Negi <mohitnegiworks@gmail.com>
2026-06-23 15:14:16 +0530

    Added feature to write and Show your task

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const submit = document.getElementById('button');
-submit.AddEventListener('click',function(){
-    
-})
+const button = document.getElementById('TaskButton');
+button.addEventListener('click',function(e){
+    e.preventDefault();
+    const taskText = button.value;
+    const newLi = document.createElement('li');
+    newLi.textContent = taskText;
+    document.getElementById('taskList').appendChild('newLi');
+});
