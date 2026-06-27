b0bcca7421902a24003edd9701a07a3e6b750822
Mohit Negi <mohitnegiworks@gmail.com>
2026-06-27 11:08:34 +0530

    Added a delete btn for each task in list

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ button.addEventListener('click',function(e){
     e.preventDefault();
     const taskText = document.getElementById('action').value;
     if(taskText.trim()===''){
-        alert("Please Enter your Tasks.");
+        alert("Please Enter your Tasks!");
         return;
     }
     const newLi = document.createElement('li');
     newLi.textContent = taskText;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '❌';
+    deleteBtn.style.marginLeft='10px';
+    deleteBtn.addEventListener('click',function(){
+        newLi.remove();
+    });
+
+    newLi.appendChild(newLi);
+
     
     const taskList=document.getElementById('taskList');
-   taskList.appendChild(newLi); 
+   taskList.appendChild(deleteBtn);
+    
 }); 
